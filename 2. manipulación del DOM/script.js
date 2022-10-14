e73860e620ecd9b07084d027bd54d6a4f5076e77
JeffersonAhimar <jeffersonahimar@outlook.com>
2022-10-14 16:34:33 -0500

    13. add eventListener

--- 2. manipulación del DOM/script.js
+++ 2. manipulación del DOM/script.js
@@ -1,40 +1,15 @@
-// h1 {color: red}
-// .parrafito { ... }
-// #pid { ... }
-
 const h1 = document.querySelector("h1");
-const p = document.querySelectorAll("p");
-const parrafito = document.getElementsByClassName("parrafito");
-const pid = document.getElementById("pid");
-const input = document.querySelector("input");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const pResult = document.querySelector("#result");
 
-console.log(input.value);
+btn.addEventListener('click', sumarInputValues);
 
-console.log({
-  h1,
-  p,
-  parrafito,
-  pid,
-  input,
-});
+function sumarInputValues(event) {
+  console.log(event);
+  event.preventDefault();
+  const sumaInputs = parseInt(input1.value) + parseInt(input2.value);
+  pResult.innerText = "Resultado: " + sumaInputs;
 
-h1.innerHTML = "Patito <br> Feo";
-h1.innerText = "Patito <br> Feo";
-// console.log(h1.getAttribute('class'));
-// h1.setAttribute('class','rojo');
-
-h1.classList.add("rojo");
-h1.classList.remove("verde");
-// h1-classList.toggle('verde')  // toggle
-// h1-classList.contains('verde') // true or false
-
-input.value = "456";
-
-const img = document.createElement("img");
-img.setAttribute(
-  "src",
-  "https://static.platzi.com/media/achievements/practico-javascript_badge-fb533fa5-73c8-4f75-9d81-0294d9013ab5.png"
-);
-console.log(img);
-pid.innerHTML="";
-pid.append(img);
+}
